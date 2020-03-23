177c68a141f9662e7504dc0674fdd8ef2c004ac7
Coral Fussman <coralfussman@gmail.com>
2020-03-23 22:21:35 +0000

    Automatically backed up by Learn

--- tester.rb
+++ tester.rb
@@ -1,4 +1,6 @@
-this_year = Time.now.year
-puts "Hey, it's not 2019!" unless this_year == 2019
-puts "hey, it's not 2020!"
+# this_year = Time.now.year
+# puts "Hey, it's not 2019!" unless this_year == 2019
+# puts "hey, it's not 2020!"
 
+[10, 20, 30, 40].select{ |num| num > 25 }
+num
